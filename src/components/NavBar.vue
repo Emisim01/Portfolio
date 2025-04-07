5c2e89b53bc3cb8a85b19cd992404d8c271d87e0
Emisim01 <emisim01@easv365.dk>
2025-04-07 20:59:33 +0200

    det meste af hero section på home

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
     <nav class="p-4 pt-6 uppercase font-bold">
         
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/protjects">Protjects</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/movies">Contact</RouterLink>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,34 +1,122 @@
 <script setup>
+import { ref } from 'vue'
+
+const marqueeText = ref(' EMILIE STRYHN SIMONSEN ')
+
+const marqueeText2 = ref(' PORTFOLIO ')
+
+// fucntion that repeats the text as a news marquee without space between each element, so it repeast itself  
 
 </script>
-
 <template>
   <main class="hero-wrapper">
-    <img src="@/assets/hero.png" alt="Hero" class="hero-img " />
-    <div class="text-white text-center font-bold text-4xl w-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex">
-      <h1>EMILIE STRYHN SIMONSEN | EMILIE STRYHN SIMONSEN | EMILIE STRYHN SIMONSEN |</h1>
-    </div>
-      <div class="text-white text-center font-bold text-4xl w-4xl absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-15 flex">
-      <p>PORTFOLIO | PORTFOLIO | PORTFOLIO |</p>
-    </div>
-  </main>
+    <div class="hero">
 
   
+    <p class="marquee pb-5">
+      <span class="text-white text-6xl font-bold pb-5 rolltextOne">
+        {{ marqueeText }}
+      </span>
+      <span class="text-white text-6xl font-bold pb-5 rolltextOne">
+        {{ marqueeText }}
+      </span>
+      <span class="text-white text-6xl font-bold pb-5 rolltextOne">
+        {{ marqueeText }}
+      </span>
+      <span class="text-white text-6xl font-bold pb-5 rolltextOne">
+        {{ marqueeText }}
+      </span>
+    </p>
+    <p class="marquee marquee2">
+      <span class="text-white text-6xl font-bold rolltextTwo">
+        {{ marqueeText2 }} 
+      </span>
+      <span class="text-white text-6xl font-bold rolltextTwo">
+        {{ marqueeText2 }} 
+      </span>
+      <span class="text-white text-6xl font-bold rolltextTwo">
+        {{ marqueeText2 }} 
+      </span>
+      <span class="text-white text-6xl font-bold rolltextTwo">
+        {{ marqueeText2 }} 
+      </span>
+      <span class="text-white text-6xl font-bold rolltextTwo">
+        {{ marqueeText2 }} 
+      </span>
+    </p>
+  </div>
+  </main>
+
+  <section class="card-section">
+    <p class="section-title text-6xl text-very-blue font-bold uppercase pt-15 ">Projects</p>
+
+
+  </section>
+
 </template>
 
 <style scoped>
+
+.rolltextOne {
+  border-right: 4px solid #fff;
+  padding: 0 10px;
+}
+
+.rolltextTwo {
+  border-right: 4px solid #fff;
+  padding: 0 10px;
+}
+
 .hero-wrapper {
   width: 100vw;
   overflow: hidden;
   position: relative;
 }
 
-.hero-img {
+.hero {
   width: 100vw;
-  height: auto;
-  display: block;
+  height: 750px;
+  background-image: url('@/assets/hero.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
 
 }
+
+.marquee {
+  margin: 0 auto;
+  white-space: nowrap;
+  overflow: hidden;
+  position: relative;
+}
+
+.marquee span {
+  display: inline-block;
+  animation: marquee 10s linear infinite;
+  white-space: nowrap;
+  margin-top: 300px;
+}
+
+.marquee2 span {
+  animation-delay: 0s;
+  margin-top: 0;
+}
+
+.section-title {
+  text-align: center;
+  margin-left: 120px;
+  margin-bottom: 70px;
+  border-bottom: 3px solid var(--color-white);
+  width: 100%
+
+}
+
+@keyframes marquee {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(-100%);
+  }
+}
 </style>
-
-
