4dab53c0eadaa34fde2d71f339b7ce86546fd62e
Emisim01 <emisim01@easv365.dk>
2025-04-07 09:20:23 +0200

    rettet lort på farver

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
 <template>
   <main class="hero-wrapper">
     <img src="@/assets/hero.png" alt="Hero" class="hero-img " />
-    <div class="text-white text-center font-bold text-4xl w-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex ">
+    <div class="text-white text-center font-bold text-4xl w-7xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex">
       <h1>EMILIE STRYHN SIMONSEN | EMILIE STRYHN SIMONSEN | EMILIE STRYHN SIMONSEN |</h1>
     </div>
       <div class="text-white text-center font-bold text-4xl w-4xl absolute top-1/2 left-1/2 transform -translate-x-1/2 translate-y-15 flex">
@@ -19,8 +19,6 @@
 <style scoped>
 .hero-wrapper {
   width: 100vw;
-  margin: 0;
-  padding: 0;
   overflow: hidden;
   position: relative;
 }
@@ -29,8 +27,7 @@
   width: 100vw;
   height: auto;
   display: block;
-  margin: 0;
-  padding: 0;
+
 }
 </style>
 
